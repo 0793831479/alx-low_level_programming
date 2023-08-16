93d3f61c4a833326b8c22510c2ba081da0f829e1
Sara <borole.sara@gmail.com>
2023-08-16 04:29:44 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,16 @@
 #include "main.h"
-/**
- * main - Entry point
- *
- * Return: Alway 0 (Sucess)
- */
 
 int main(void)
 {
-	char *h = "main\n";
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
 
-	while (*h)
-	{
-		_putchar(*h);
-		h++;
-	}
-	return (0);
+    return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,9 @@
-#ifndef MAIN_HEADER_FILE
-#define MAIN_HEADER_FIE
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_x10(void;)
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -13,4 +13,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-#endif
+
+#endif /* MAIN_H */
