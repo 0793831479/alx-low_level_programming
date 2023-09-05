a43f37cacef01b561032e6351804ba63be793136
Sara <borole.sara@gmail.com>
2023-09-05 03:59:46 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,40 +1,31 @@
 #include "main.h"
 #include <stdlib.h>
-
-void util(char **, char *);
-void create_word(char **, char *, int, int, int);
-
+#include <stdio.h>
+#include <string.h>
 /**
- * strtow - splits a string into words.
- * @str: the string
+ * strtow - concatenates arguments.
+ * @str: String to be splitted.
  *
- * Return: returns a pointer to an array of strings (words)
+ * Return: a pointer to array of String.
  */
 char **strtow(char *str)
 {
-	int i, flag, len;
-	char **words;
+	char *array = NULL;
+	unsigned int i = 0, j = 0, k;
 
-	if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
+	if (strncmp(str, "", 1) || str == NULL)
 		return (NULL);
-
-	i = flag = len = 0;
-	while (str[i])
+	array = malloc((i + j + 1) * sizeof(char));
+	if (array == NULL)
+		return (NULL);
+	for (k = 0; k < i; k++)
+		array[k] = str[k];
+	i = k;
+	for (k = 0; k < j; k++)
 	{
-		if (flag == 0 && str[i] != ' ')
-			flag = 1;
-		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
-		{
-			flag = 0;
-			len++;
-		}
+		array[i] = str[k];
 		i++;
 	}
-
-	len += flag == 1 ? 1 : 0;
-	if (len == 0)
-		return (NULL);
-
-	words = (char **)malloc(sizeof(char *) * (len + 1));
-	if (words == NULL)
-		return (NULL);
+	array[i] = '\0';
+	return (NULL);
+}
